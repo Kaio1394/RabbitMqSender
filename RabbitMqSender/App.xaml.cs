79a1a8f26db5f0bfb1853c4a88474670dd9d5684
Kaio <kaio.santiago.13@hotmail.com>
2024-12-23 12:19:03 -0300

    Add project files.

---
+++ RabbitMqSender/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace RabbitMqSender
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
